9510bb181eeea60f46243ff2bb881a063f90f3aa
Simon Cook <simon.cook@trademe.co.nz>
2014-07-28 14:07:39 +1200

    Fixed issue with banners rotating when presented on top of view controllers that don’t support rotation.  Banners will only rotate if the keyWindows rootViewController or the presentedViewController (if it exists) support it.

--- Classes/TWMessageBarManager.m
+++ Classes/TWMessageBarManager.m
@@ -795,7 +795,7 @@ static UIColor *kTWDefaultMessageBarStyleSheetInfoStrokeColor = nil;
 
 - (CGRect)orientFrame:(CGRect)frame
 {
-    if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation) || UIDeviceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation))
+    if (UIDeviceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation))
     {
         frame = CGRectMake(frame.origin.x, frame.origin.y, frame.size.height, frame.size.width);
     }
@@ -955,6 +955,17 @@ static UIColor *kTWDefaultMessageBarStyleSheetInfoStrokeColor = nil;
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
     return self.statusBarStyle;
+}
+
+- (BOOL)shouldAutorotate {
+    UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    if (rootViewController.presentedViewController) {
+        return [rootViewController.presentedViewController shouldAutorotate];
+    }
+    else {
+        return [rootViewController shouldAutorotate];
+    }
 }
 
 @end
